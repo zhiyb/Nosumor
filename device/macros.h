6ebd6f73b98676cdfff036f2a803c1902aa9dfc6
Yubo Zhi <normanzyb@gmail.com>
2018-01-03 04:19:45 +0000

    device/macros: Static assertion

--- device/macros.h
+++ device/macros.h
@@ -3,6 +3,22 @@
 
 #include <stm32f7xx.h>
 
+// http://www.pixelbeat.org/programming/gcc/static_assert.html
+#define ASSERT_CONCAT_(a, b) a##b
+#define ASSERT_CONCAT(a, b) ASSERT_CONCAT_(a, b)
+/* These can't be used after statements in c89. */
+#ifdef __COUNTER__
+  #define STATIC_ASSERT(e,m) \
+    ;enum { ASSERT_CONCAT(static_assert_, __COUNTER__) = 1/(int)(!!(e)) }
+#else
+  /* This can't be used twice on the same line so ensure if using in headers
+   * that the headers are not included twice (by wrapping in #ifndef...#endif)
+   * Note it doesn't cause an issue when used on same line of separate modules
+   * compiled with gcc -combine -fwhole-program.  */
+  #define STATIC_ASSERT(e,m) \
+    ;enum { ASSERT_CONCAT(assert_line_, __LINE__) = 1/(int)(!!(e)) }
+#endif
+
 #define PACKED		__attribute__((packed))
 #define ALIGN(b)	__attribute__((aligned(b)))
 #define ALIGNED		__attribute__((aligned))
