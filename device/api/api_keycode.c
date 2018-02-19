37d0960364e9f7a60307e7d3fbaa6d945a569417
Yubo Zhi <normanzyb@gmail.com>
2018-02-19 21:01:20 +0000

    device/api/api_keycode: Report key count, keycode and key names

--- device/api/api_keycode.c
+++ device/api/api_keycode.c
@@ -9,17 +9,40 @@
 static void handler(void *hid, uint8_t channel,
 		    void *data, uint8_t size, void *payload);
 const struct api_reg_t api_keycode = {
-	handler, 0x0001, "Keycode"
+	handler, 0x0002, "Keycode"
 };
 
 static void handler(void *hid, uint8_t channel,
 		    void *data, uint8_t size, void *payload)
 {
-	if (size != 2) {
+	if (size > 2) {
 		printf(ESC_ERROR "[KEYCODE] Invalid size: %u\n", size);
 		return;
 	}
 
-	api_keycode_t *p = (api_keycode_t *)data;
-	keyboard_keycode_set(p->btn, p->code);
+	uint8_t *dp = data;
+	uint8_t *pp = payload;
+	switch (size) {
+	case 0:
+		// Report key count
+		*pp = KEYBOARD_KEYS;
+		api_send(hid, channel, 1u);
+		break;
+	case 1:	{
+		// Report keycode and key name
+		uint8_t btn = *dp;
+		*pp++ = keyboard_keycode(btn);
+		const char *cp = keyboard_name(btn);
+		uint32_t len = strlen(cp);
+		memcpy(pp, cp, len);
+		api_send(hid, channel, len + 1u);
+		break;
+	}
+	case 2: {
+		// Set keycode
+		api_keycode_t *p = (api_keycode_t *)dp;
+		keyboard_keycode_set(p->btn, p->code);
+		break;
+	}
+	}
 }
